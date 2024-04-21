cc21962e3cf033f52792fca56fb0ec4adbd6b9a6
Guilherme Varela <guilherme.varelaaa@gmail.com>
2024-04-21 20:16:29 +0100

    query 3 e 4 otimizadas

--- analytical/Q3.sql
+++ analytical/Q3.sql
@@ -1,4 +1,5 @@
-SELECT tagname, round(avg(total), 3), count(*)
+EXPLAIN ANALYZE
+SELECT t.tagname, round(avg(t.total), 3), count(*)
 FROM (
     SELECT t.tagname, q.id, count(*) AS total
     FROM tags t
@@ -14,6 +15,89 @@ FROM (
         HAVING count(*) > 10
     )
     GROUP BY t.tagname, q.id
-)
+) t
 GROUP BY tagname
 ORDER BY 2 DESC, 3 DESC, tagname;
+
+
+-- QUESTIONS
+-- QUESTIONSTAGS
+-- TAGS
+-- ANSWERS
+
+
+-- 25247.67 ms
+------------------------------------------------------------------------
+
+EXPLAIN ANALYZE
+SELECT t.tagname, round(avg(t.total), 3), count(*)
+FROM (
+    SELECT t.tagname, q.id, COUNT(*) AS total
+    FROM RelevantTags t
+    JOIN questionstags qt ON qt.tagid = t.id
+    JOIN questions q ON q.id = qt.questionid
+    LEFT JOIN answers a ON a.parentid = q.id
+    GROUP BY t.tagname, q.id
+) t
+GROUP BY tagname
+ORDER BY 2 DESC, 3 DESC, tagname;
+
+
+------------------------------------------------------------------------
+
+
+CREATE MATERIALIZED VIEW RelevantTags AS
+SELECT t.id, t.tagname
+FROM tags t
+JOIN questionstags qt ON qt.tagid = t.id
+JOIN questions q ON q.id = qt.questionid
+GROUP BY t.id
+HAVING COUNT(*) > 10;
+
+SELECT rt.tagname, ROUND(AVG(a.total), 3) AS avg_total, COUNT(*) AS num_questions
+FROM RelevantTags rt
+JOIN questionstags qt ON qt.tagid = rt.id
+JOIN questions q ON q.id = qt.questionid
+LEFT JOIN 
+    (SELECT parentid, COUNT(*) AS total
+     FROM answers
+     GROUP BY parentid) a ON q.id = a.parentid
+GROUP BY rt.tagname
+ORDER BY avg_total DESC, num_questions DESC, rt.tagname;
+
+
+------------------------------------------------------------------------
+WITH tag_question_occurrences AS (
+    SELECT rt.tagname, COUNT(DISTINCT qt.questionid) AS nr_tags, COUNT(*) AS total
+    FROM RelevantTags rt
+    JOIN questionstags qt ON qt.tagid = rt.id
+    LEFT JOIN answers a ON a.parentid = qt.questionid
+    GROUP BY rt.tagname
+)
+SELECT t.tagname, ROUND((t.total :: NUMERIC/ t.nr_tags), 3) AS avg_total, t.nr_tags
+FROM tag_question_occurrences t
+ORDER BY avg_total DESC, t.nr_tags DESC, t.tagname;
+
+------------------------------------------------------------------------
+
+WITH tag_question_occurrences AS (
+    SELECT a.tagname, COUNT(*) as nr_tags
+    FROM (
+        SELECT qt.questionid, rt.tagname
+        FROM RelevantTags rt
+        JOIN questionstags qt ON qt.tagid = rt.id 
+        GROUP BY rt.tagname, qt.questionid
+    ) a
+    GROUP BY a.tagname
+),
+tag_occurrences AS (
+    SELECT rt.tagname, COUNT(*) AS total
+    FROM RelevantTags rt
+    JOIN questionstags qt ON qt.tagid = rt.id 
+    LEFT JOIN answers a ON a.parentid = qt.questionid
+    GROUP BY rt.tagname
+)
+SELECT t.tagname, ROUND((t.total :: NUMERIC/ tq.nr_tags), 3) AS avg_total, tq.nr_tags
+FROM tag_occurrences t
+JOIN tag_question_occurrences tq ON t.tagname = tq.tagname 
+ORDER BY avg_total DESC, tq.nr_tags DESC, t.tagname;
--- analytical/Q4.sql
+++ analytical/Q4.sql
@@ -1,3 +1,4 @@
+EXPLAIN ANALYZE 
 SELECT date_bin('1 minute', date, '2008-01-01 00:00:00'), count(*)
 FROM badges
 WHERE NOT tagbased
@@ -14,3 +15,34 @@ WHERE NOT tagbased
     AND userid <> -1
 GROUP BY 1
 ORDER BY 1;
+
+
+------------------------------------------------------------------------------------------------------------
+
+EXPLAIN ANALYZE
+SELECT date_trunc('minute', date) AS minute, count(*)
+FROM badges
+WHERE NOT tagbased
+    AND name NOT IN (
+        'Analytical',
+        'Census',
+        'Documentation Beta',
+        'Documentation Pioneer',
+        'Documentation User',
+        'Reversal',
+        'Tumbleweed'
+    )
+    AND class in (1, 2, 3)
+    AND userid <> -1
+GROUP BY minute
+ORDER BY minute;
+
+
+CREATE INDEX idx_badges_date ON badges (date);
+CREATE INDEX idx_badges_tagbased ON badges (tagbased);
+CREATE INDEX idx_badges_name ON badges (name);
+CREATE INDEX idx_badges_class ON badges (class);
+CREATE INDEX idx_badges_userid ON badges (userid);
+
+
+
