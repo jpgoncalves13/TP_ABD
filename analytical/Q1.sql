3aa96c7812e8f13bbcb221249f48a7c29fca97cc
GVarelaa <guilherme.varelaaa@gmail.com>
2024-04-23 23:32:31 +0100

    q1 e q2 otimizadas

--- analytical/Q1.sql
+++ analytical/Q1.sql
@@ -60,4 +60,37 @@ CREATE INDEX idx_comments ON comments (userid, creationdate);
 CREATE INDEX idx_users on users (id);
 
 
--- VISTAS MATERIALIZADAS ??
+-----------------------------------------------------------------------------------------------------------------------------
+
+EXPLAIN ANALYZE
+SELECT u.id, u.displayname, COALESCE(total_activities, 0) AS total
+FROM users u
+LEFT JOIN (
+    SELECT owneruserid, COUNT(*) AS total_activities
+    FROM (
+        SELECT owneruserid FROM questions WHERE creationdate BETWEEN NOW() - INTERVAL '6 months' AND NOW()
+        UNION ALL
+        SELECT owneruserid FROM answers WHERE creationdate BETWEEN NOW() - INTERVAL '6 months' AND NOW()
+        UNION ALL
+        SELECT userid FROM comments WHERE creationdate BETWEEN NOW() - INTERVAL '6 months' AND NOW()
+    )
+    GROUP BY owneruserid
+) activity ON activity.owneruserid = u.id
+ORDER BY total DESC
+LIMIT 100;
+
+-- 3200 ms
+
+CREATE INDEX idx_questions_owneruserid ON questions(owneruserid);
+CREATE INDEX idx_questions_creationdate ON questions(creationdate);
+
+CREATE INDEX idx_answers_owneruserid ON answers(owneruserid);
+CREATE INDEX idx_answers_creationdate ON answers(creationdate);
+
+CREATE INDEX idx_comments_userid ON comments(userid);
+CREATE INDEX idx_comments_creationdate ON comments(creationdate);
+
+CREATE INDEX idx_users_id ON users(id);
+
+
+-- 400 ms 
